6e9eee185cd51514070e88b36016aed7112e6dcf
rladbsxo2022040033 <jungryeok168@naver.com>
2026-01-31 19:02:51 +0900

    타이틀 브금 및 효과음 추가

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -18,12 +18,15 @@ public class GameManager : MonoBehaviour
     public float expMultiplier = 1.0f; //경험치 추가배율
     public LevelUp uiLevelUp; //레벨업 시 연결할 UI변수
     public float damageBuff = 1.0f; //데미지 관리 기본 1배
-
+    
     public Slider expSlider;
     public TextMeshProUGUI timerText;
     public TextMeshProUGUI killText;
     public TextMeshProUGUI levelText;
 
+    public AudioSource titleBgm;//타이틀 BGM연결 
+    public AudioSource levelUpSfx;//레벨업 시 효과음 
+    public AudioSource expGetSfx;//보석획득 효과음
     public GameObject uiGameOver;
 
     void Awake()
@@ -34,7 +37,11 @@ public class GameManager : MonoBehaviour
     }
 
     public void GameStart()
-    {
+    {   
+        if (titleBgm != null) { //게임 시작 누르면 타이틀BGM종료
+            titleBgm.Stop();
+        }
+
         Time.timeScale = 1f;
         isPlayerLive = true;
         level = 1;
@@ -59,6 +66,9 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
+        if (titleBgm != null) { //게임 실행시 BGM재생
+            titleBgm.Play();
+        }
         // 초기 UI 업데이트
         level = 1;
         exp = 0;
@@ -81,7 +91,11 @@ public class GameManager : MonoBehaviour
     }
 
     public void GetExp()
-    {
+    {   
+        if (expGetSfx != null) {
+            expGetSfx.PlayOneShot(expGetSfx.clip); 
+        }
+
         exp += Mathf.RoundToInt(1 * expMultiplier);
         //필요한 경험치량 증가
         int targetExp = Mathf.RoundToInt(Mathf.Pow(level, 1.4f)) + 9;
@@ -89,11 +103,14 @@ public class GameManager : MonoBehaviour
         if (exp >= targetExp) {
             level++;
             exp = 0;
+            if (levelUpSfx != null) {
+                levelUpSfx.Play(); 
+            }
             UpdateLevelUI();
             
-            if (level == 2) {
+            if (level == 9) {
                 player.Evolve(1); // 파이숭이 진화
-            } else if (level == 3) {
+            } else if (level == 18) {
                 player.Evolve(2); // 초염몽 진화
             }
 
